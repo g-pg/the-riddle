536b303c16ea9c6461dcba2d5d55a4cc8f2f58e1
g-pg <gabrielpgusso@gmail.com>
2023-02-05 23:43:12 -0300

    added typewriter effect

--- src/components/GameWon.jsx
+++ src/components/GameWon.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Typewriter from "typewriter-effect";
 
 export default function GameWon(props) {
+	const textWon = "Você desvendou o enigma.";
+	const subTextWon = "Por enquanto...";
+
 	return (
 		<section className="gamewon-page">
 			<div className="gamewon--text">
-				<h1>Você desvendou o enigma.</h1>
-				<p>Por enquanto...</p>
+				<h1>
+					<Typewriter
+						onInit={(typewriter) => {
+							typewriter
+
+								.changeDelay(80)
+								.typeString(textWon)
+								.pauseFor(1500)
+								.deleteAll()
+								.changeDelay(150)
+								.typeString(subTextWon)
+								.start();
+						}}
+					/>
+				</h1>
 			</div>
 		</section>
 	);
